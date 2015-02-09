186728adeafc714aabc972ed3dd2a4b11d134d32
duffield44 <duffieldbenjamin44@gmail.com>
2015-02-09 20:37:19 +0800

    Add newGame() function to reset the random number and elements

--- js/app.js
+++ js/app.js
@@ -12,6 +12,39 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*------- 'New Game' button Functionality -----------*/
+
+  	/* Click 'New Game' to invoke New Game function */ 
+  	$('.new').click(function(){
+  		console.log('Clicked .new');
+  		newGame();
+  	})
+
+  	var num = 0;
+
+  	/* newGame function invoked by clicking 'New Game' */
+  	function newGame() {
+
+  		/* Generate new random number */
+  		var num = Math.floor((Math.random() * 100) + 1);
+  		console.log(num);
+
+  		/* Return h2#feedback to original text */
+  		setFeedback("Make Your Guess");
+  		/* Return span#count to 0 */
+  		/* Clear ul#guessList */
+  		$('#guessList').empty();
+
+  	}
+
+
+  	
+
+  	/* Setting feedback type */
+  	function setFeedback(feedback) {
+  		$('#feedback').text(feedback);
+  	}
+
 });
 
 
